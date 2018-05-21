c76379488610be8192d1687dd9bbe6a9ad073e17
vyshiva <elena_haponyuk@ukr.net>
2018-05-21 20:20:14 +0300

    Open and close functionalities were added to form

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
 const rimraf = require('rimraf');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps')
 
@@ -32,15 +34,27 @@ gulp.task('templates:compile', function buildHTML() {
 /* -------Styles compile --------- */
 gulp.task('styles:compile', function () {
   return gulp.src('source/styles/main.scss')
-    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename('main.min.css'))
-    .pipe(sourcemaps.write())
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(gulp.dest('build/css'));
+});
+
+/* -------JS --------- */
+gulp.task('js', function() {
+  return gulp.src([
+    'source/js/form.js',
+    'source/js/navigation.js',
+    'source/js/main.js'
+  ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/js'));
 });
 
 /* -------Sprite --------- */
@@ -79,11 +93,12 @@ gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 gulp.task('watch', function() {
   gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
   gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+  gulp.watch('source/js/**/*.js', gulp.series('js'));
 });
 
 gulp.task('default', gulp.series(
   'clean',
-  gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+  gulp.parallel('templates:compile', 'styles:compile', 'js', 'sprite', 'copy'),
   gulp.parallel('watch', 'server')
   )
 );
